64adc27eb98bb05e3ca01e321c66a29e9af58c1e
Artur K <artur.kriveckiy@yandex.ru>
2021-12-10 00:00:00 +0300

    Add function MyMul()

---
+++ funcs.cpp
@@ -0,0 +1,7 @@
+//funcs.cpp
+#include "stdafx.h"
+#include "funcs.h"
+
+double MyMul(double a, double b) {
+return a * b;
+}
---
+++ program.cpp
@@ -0,0 +1,10 @@
+//файл program.cpp
+#include "stdafx.h"
+#include <iostream>
+#include "funcs.h"
+using namespace std;
+int main() {
+cout << MyMul(10, 3) << endl;
+system("pause");
+return 0;
+}
